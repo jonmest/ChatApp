2d92ecca6d9331c4dbe124b4751b7d3b095cdc76
jonmest <jc222rz@student.lnu.se>
2019-12-02 13:34:52 +0100

    Setting up config

---
+++ app/db/index.js
@@ -0,0 +1,7 @@
+'use strict'
+const config = require('..config')
+const Mongoose = require('mongoose').connect(config.dbURI)
+
+Mongoose.connection.on('error', error => {
+    console.log('MongoDB Error: ', error)
+})
